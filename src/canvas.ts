98ad0a08123aaf93f0af52951aa5a5de76fdc50c
Gektor Gevorgyan <gektor.gevorgyan@gmail.com>
2023-12-14 15:50:05 +0400

    Changed some styles. Added some physics(Not completed)

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,127 +1,221 @@
-function randomIntFromRange(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+  export function canvasInit() {
+    // Check if params exits in local storage. If not init defaults values
+    if (!localStorage.getItem("color")) localStorage.setItem("color", "#9C27B0");
+    if (!localStorage.getItem("radius")) localStorage.setItem("radius", "50");
+    if (!localStorage.getItem("elasticity"))
+      localStorage.setItem("elasticity", "50");
 
-function randomColor(colors: string[]): string {
-  return colors[Math.floor(Math.random() * colors.length)];
-}
+    // get inputs elements
+    const colorInput = document.getElementById("color");
+    const radiusInput = document.getElementById("radius");
+    const elasticityInput = document.getElementById("elasticity");
+    const radiusOutput = document.getElementById("radiusOutput");
+    const elasticityOutput = document.getElementById("elasticityOutput");
 
-export function canvasInit() {
-  const canvas = <HTMLCanvasElement>document.getElementById("gameFrame");
-  const context = canvas.getContext("2d");
+    // Init inputs values from localStorage
+    (colorInput as HTMLInputElement).value = localStorage.getItem("color");
+    (radiusInput as HTMLInputElement).valueAsNumber =
+      +localStorage.getItem("radius");
+    (elasticityInput as HTMLInputElement).valueAsNumber =
+      +localStorage.getItem("elasticity");
+    radiusOutput.innerHTML = localStorage.getItem("radius");
+    elasticityOutput.innerHTML = localStorage.getItem("elasticity");
 
-  // Init mouse position as default
-  const mouse: { x: number; y: number } = {
-    x: innerWidth / 2,
-    y: innerHeight / 2,
-  };
+    // Add events listeners for handle changes and save in localstorage
+    colorInput.addEventListener("input", (event) => {
+      const { target } = event;
+      localStorage.setItem("color", (target as HTMLInputElement).value);
+      console.log("Color: ", localStorage.getItem("color"));
+    });
+    radiusInput.addEventListener("input", (event) => {
+      const { target } = event;
+      localStorage.setItem(
+        "radius",
+        (target as HTMLInputElement).valueAsNumber.toString()
+      );
+      radiusOutput.innerHTML = localStorage.getItem("radius");
+      console.log("Radius: ", localStorage.getItem("radius"));
+    });
+    elasticityInput.addEventListener("input", (event) => {
+      const { target } = event;
+      localStorage.setItem(
+        "elasticity",
+        (target as HTMLInputElement).valueAsNumber.toString()
+      );
+      elasticityOutput.innerHTML = localStorage.getItem("elasticity");
+      console.log("Elasticity: ", localStorage.getItem("elasticity"));
+    });
 
-  // Init possible colors
-  const colors: string[] = ["#2185C5", "#7ECEFD", "#FFF6E5", "#FF7F66"];
-  const gravity = 1;
-  const friction = 0.95;
-  const ballArray = [];
+    const canvas = <HTMLCanvasElement>document.getElementById("gameFrame");
+    const context = canvas.getContext("2d");
 
-  // Resize the canvas to fill browser window dynamically
-  window.addEventListener("resize", resizeCanvas, false);
+    // Init mouse position as default
+    const mouse: { x: number; y: number } = {
+      x: innerWidth / 2,
+      y: innerHeight / 2,
+    };
 
-  function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  }
+    // Init const gravity, ballId and ballArray;
+    const GRAVITY = 0.098;
+    var ballId = 0;
+    const ballArray: any = [];
 
-  resizeCanvas();
+    // Resize the canvas to fill browser window dynamically
+    window.addEventListener("resize", resizeCanvas, false);
 
-  // Mouse move event
-  window.addEventListener("mousemove", function (event) {
-    mouse.x = event.clientX;
-    mouse.y = event.clientY;
-  });
+    function resizeCanvas() {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    }
 
-  // Mouse click event and create ball
-  window.addEventListener("click", function (event) {
- 
-    const { target } = event;
-    
-    if((target as HTMLElement).id === "gameFrame"){
-        var radius = randomIntFromRange(10, 30);
-        var color = randomColor(colors);
+    resizeCanvas();
+
+    // Mouse move event
+    window.addEventListener("mousemove", function (event) {
+      mouse.x = event.clientX;
+      mouse.y = event.clientY;
+    });
+
+    // Mouse click event and create ball
+    window.addEventListener("click", function (event) {
+      const { target } = event;
+
+      if ((target as HTMLElement).id === "gameFrame") {
+        var color = localStorage.getItem("color");
+        var radius = +localStorage.getItem("radius");
+        var elasticity = +localStorage.getItem("elasticity");
         var x = event.clientX;
         var y = event.clientY;
-        var dx = randomIntFromRange(-2, 2);
-        var dy = randomIntFromRange(-2, 2);
+        var dx = 0;
+        var dy = 0;
         if (ballArray.length >= 15) ballArray.shift();
-        ballArray.push(new Ball(x, y, dx, dy, radius, color));
+        ballArray.push(
+          new Ball(ballId++, x, y, dx, dy, radius, elasticity, color)
+        );
+      }
+    });
+
+    function Ball(
+      ballId: number,
+      x: number,
+      y: number,
+      dx: number,
+      dy: number,
+      radius: number,
+      elasticity: number,
+      color: string
+    ) {
+      this.ballId = ballId;
+      this.x = x;
+      this.y = y;
+      this.dx = dx;
+      this.dy = dy;
+      this.radius = radius;
+      this.elasticity = elasticity / 100;
+      this.color = color;
+
+      this.update = function (deltaTime: number) {
+        if (this.y + this.radius + this.dy > canvas.height) {
+          this.dy = -this.dy * this.elasticity;
+        } else {
+          this.dy += GRAVITY * deltaTime;
+        }
+
+        if (
+          this.x + this.radius + this.dx > canvas.width ||
+          this.x - this.radius < 0
+        ) {
+          this.dx = -this.dx;
+        } else {
+          this.dx *= this.elasticity;
+        }
+
+        if (this.dy < 0.1 && this.dy > -0.1) this.dy = 0;
+        if (this.dx < 0.1 && this.dx > -0.1) this.dx = 0;
+
+        ballArray.find((ball) => {
+          var distance = Math.hypot(ball.x - this.x, ball.y - this.y);
+
+          if (
+            ball.ballId !== this.ballId &&
+            distance <= ball.radius + this.radius
+          ) {
+            //console.log("hitted!", distance);
+            //console.log(ball.x, this.x)
+
+            if (ball.x > this.x){
+              ball.dx += elasticity / 10;
+              this.dx -= elasticity / 10;
+            } 
+            else {
+              ball.dx -= elasticity / 10;
+              this.dx += elasticity / 10;
+            }
+
+            ball.dy = -elasticity / 10;
+            this.dy = -this.dy
+            return ball;
+          }
+        });
+
+        // console.log(
+        //   "dx: ",
+        //   this.dx,
+        //   "dy: ",
+        //   this.dy,
+        //   "x: ",
+        //   this.x,
+        //   "y: ",
+        //   this.y
+        // );
+
+        this.x += this.dx;
+        this.y += this.dy;
+
+        this.draw();
+      };
+
+      this.draw = function () {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        context.fillStyle = this.color;
+        context.fill();
+        context.closePath();
+      };
     }
-  });
 
-  function Ball(
-    x: number,
-    y: number,
-    dx: number,
-    dy: number,
-    radius: number,
-    color: string
-  ) {
-    this.x = x;
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
-    this.radius = radius;
-    this.color = color;
+    var lastTime = 0;
 
-    this.update = function () {
-      if (this.y + this.radius + this.dy > canvas.height) {
-        this.dy = -this.dy * friction;
-      } else {
-        this.dy += gravity;
+    function tick(currentTime: number) {
+      const deltaTime = currentTime - lastTime;
+      // ... update game elements using deltaTime
+
+      if (isNaN(deltaTime)) return;
+
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      for (var i = 0; i < ballArray.length; i++) {
+        ballArray[i].update(deltaTime);
       }
 
-      if (
-        this.x + this.radius + this.dx > canvas.width ||
-        this.x - this.radius < 0
-      ) {
-        this.dx = -this.dx;
-      } else {
-        this.dx *= friction;
-      }
+      lastTime = currentTime;
+      requestAnimationFrame(tick);
 
-      this.x += this.dx;
-      this.y += this.dy;
-      this.draw();
-    };
-
-    this.draw = function () {
       context.beginPath();
-      context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      context.fillStyle = this.color;
+      context.arc(
+        mouse.x,
+        mouse.y,
+        +localStorage.getItem("radius"),
+        0,
+        Math.PI * 2,
+        false
+      );
+      context.fillStyle = "#37474F";
       context.fill();
+      context.stroke();
+      context.strokeStyle = localStorage.getItem("color");
+      context.lineWidth = 3;
       context.closePath();
-    };
-  }
-
-var lastTime = 0;
-
-  function tick(currentTime: number) {
-    const deltaTime = currentTime - lastTime;
-    // ... update game elements using deltaTime
-
-
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    for (var i = 0; i < ballArray.length; i++) {
-      ballArray[i].update();
     }
 
-    context.beginPath();
-    context.arc(mouse.x, mouse.y, 30, 0, Math.PI * 2, false);
-    context.fillStyle = "#37474F";
-    context.fill();
-    context.closePath();
-    
-
-    lastTime = currentTime;
-    requestAnimationFrame(tick);    
+    tick(lastTime);
   }
-
-  tick(lastTime);
-}
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,9 @@
 import { canvasInit } from "./canvas";
 import "./index.css";
 
-const root = document.getElementById("root");
-  
-root.innerHTML = `
-<canvas id="gameFrame">
-Your browser does not support the HTML canvas tag.
-</canvas> 
-<header>
-<h1>Bouncing Ballz</h1>
-<form>
-<div class="form-group">
-<label for="color">Color:</label>
-<input type="color" id="color" name="color"> 
-</div>
-<div class="form-group">
-<label for="radius">Radius (between 0 and 100 in px):</label>
-<input type="range" id="radius" name="radius" min="0" max="100">
-</div>
-<div class="form-group">
-<label for="elasticity">Elasticity (between 0 and 100 in %):</label>
-<input type="range" id="elasticity" name="elasticity" min="0" max="100"> 
-</div>
-</form>
-</header>`;
+const canvasFrame = document.getElementById("canvas-frame");
 
-canvasInit()
+canvasFrame.innerHTML =
+  "<canvas id='gameFrame'>Your browser does not support the HTML canvas tag.</canvas>";
+
+canvasInit();
